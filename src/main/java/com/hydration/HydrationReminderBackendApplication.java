745a3378792b5221b0dd3f13f73ee8e8a4b1653c
Sunny <sunnysinghhrana@gmail.com>
2026-06-04 23:14:07 +0530

    -initial commit

---
+++ src/main/java/com/hydration/HydrationReminderBackendApplication.java
@@ -0,0 +1,13 @@
+package com.hydration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HydrationReminderBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HydrationReminderBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hydration/HydrationReminderBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hydration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HydrationReminderBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
