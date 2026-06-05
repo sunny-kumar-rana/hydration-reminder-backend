5ea1f07a48d9a5c8311315a8fde013d0eecb47bd
Sunny <sunnysinghhrana@gmail.com>
2026-06-05 13:09:50 +0530

    -create UserRepository

---
+++ src/main/java/com/hydration/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.hydration.repository;
+
+import com.hydration.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    boolean existsByUsername(String username);
+    boolean existsByEmail(String email);
+}
