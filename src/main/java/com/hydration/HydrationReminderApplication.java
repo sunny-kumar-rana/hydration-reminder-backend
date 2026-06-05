e5135eec2e1df8d0ad043441396fa9a21edc5bbd
Sunny <sunnysinghhrana@gmail.com>
2026-06-05 11:50:40 +0530

    -name correction

--- src/main/java/com/hydration/HydrationReminderApplication.java
+++ src/main/java/com/hydration/HydrationReminderApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HydrationReminderBackendApplication {
+public class HydrationReminderApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(HydrationReminderBackendApplication.class, args);
+        SpringApplication.run(HydrationReminderApplication.class, args);
     }
 
 }
