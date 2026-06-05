6636623385fa0720f642088cf2cd92859b7dd114
Sunny <sunnysinghhrana@gmail.com>
2026-06-05 22:13:26 +0530

    -create UserService

---
+++ src/main/java/com/hydration/service/UserService.java
@@ -0,0 +1,36 @@
+package com.hydration.service;
+
+import com.hydration.dto.RegisterRequest;
+import com.hydration.dto.RegisterResponse;
+import com.hydration.entity.User;
+import com.hydration.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+    public RegisterResponse register(RegisterRequest request){
+        if(userRepository.existsByUsername(request.getUsername())){
+            throw new RuntimeException("username already exists");
+        }if(userRepository.existsByEmail(request.getEmail())){
+            throw new RuntimeException("e-mail already exists");
+        }
+        User user = new User();
+        user.setUsername(request.getUsername());
+        user.setEmail(request.getEmail());
+        user.setPassword(request.getPassword());
+        userRepository.save(user);
+
+        RegisterResponse response = new RegisterResponse();
+        response.setUsername(user.getUsername());
+        response.setMessage("User Registered Successfully");
+
+        return response;
+    }
+}
