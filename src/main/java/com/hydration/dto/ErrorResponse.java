196636c9f1344e379e556dd77fcfa69dd13379d0
Sunny <sunnysinghhrana@gmail.com>
2026-06-06 23:59:21 +0530

    -add ErrorResponse class

---
+++ src/main/java/com/hydration/dto/ErrorResponse.java
@@ -0,0 +1,13 @@
+package com.hydration.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class ErrorResponse {
+    private String message;
+    private int status;
+}
