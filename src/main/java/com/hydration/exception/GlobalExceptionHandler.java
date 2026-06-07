d9eb4d0b343db0b587839a49e5a071c8d38ec359
Sunny <sunnysinghhrana@gmail.com>
2026-06-07 23:20:15 +0530

    -add methodArgumentNotValidException handler method ing GlobalExceptionHandler class

--- src/main/java/com/hydration/exception/GlobalExceptionHandler.java
+++ src/main/java/com/hydration/exception/GlobalExceptionHandler.java
@@ -4,8 +4,12 @@ import com.hydration.dto.ErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -25,5 +29,14 @@ public class GlobalExceptionHandler {
         response.setStatus(HttpStatus.CONFLICT.value());
 
         return new ResponseEntity<>(response, HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String,String>> handleMethodArgumentNotValid(MethodArgumentNotValidException exception){
+        Map<String, String> response = new HashMap<>();
+
+        exception.getBindingResult().getFieldErrors().forEach(error -> response.put(error.getField(), error.getDefaultMessage()));
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
     }
 }
