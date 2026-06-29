c146e8a077a1311276262ce792be3e9ae6f89b24
Sunny <sunnysinghhrana@gmail.com>
2026-06-29 23:43:09 +0530

    -add LoginRequest class

---
+++ src/main/java/com/hydration/dto/LoginRequest.java
@@ -0,0 +1,23 @@
+package com.hydration.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginRequest {
+
+    @NotBlank
+    @Size(min = 5, max = 20)
+    private String username;
+
+    @NotBlank
+    @Size(min = 6, max = 16)
+    private String password;
+}
