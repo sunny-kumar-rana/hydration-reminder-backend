6712d4a57e4bc927e2b421b773036f32b1d66574
Sunny <sunnysinghhrana@gmail.com>
2026-06-30 23:31:23 +0530

    -add method in repository find by username

--- src/main/java/com/hydration/repository/UserRepository.java
+++ src/main/java/com/hydration/repository/UserRepository.java
@@ -3,7 +3,11 @@ package com.hydration.repository;
 import com.hydration.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByUsername(String username);
     boolean existsByEmail(String email);
+
+    Optional<User> findByUsername(String username);
 }
