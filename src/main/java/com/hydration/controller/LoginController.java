9757383414cbf6964bb9b64490ccc6bb5adf6eea
Sunny <sunnysinghhrana@gmail.com>
2026-07-12 23:45:12 +0530

    -add Login api

---
+++ src/main/java/com/hydration/controller/LoginController.java
@@ -0,0 +1,29 @@
+package com.hydration.controller;
+
+import com.hydration.dto.LoginRequest;
+import com.hydration.dto.LoginResponse;
+import com.hydration.service.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class LoginController {
+    private final UserService userService;
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> login(
+            @Valid @RequestBody LoginRequest request) {
+
+        LoginResponse response = userService.login(request);
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(response);
+    }
+}
