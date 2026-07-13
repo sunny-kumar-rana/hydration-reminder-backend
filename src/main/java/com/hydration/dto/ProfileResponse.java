ded793704152bdafb73d5e49369d80c81ae10664
Sunny <sunnysinghhrana@gmail.com>
2026-07-13 23:52:59 +0530

    -add fields to ProfileResponse dto

--- src/main/java/com/hydration/dto/ProfileResponse.java
+++ src/main/java/com/hydration/dto/ProfileResponse.java
@@ -1,4 +1,23 @@
 package com.hydration.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProfileResponse {
+    private String username;
+    private String email;
+    private Integer dailyGoal;
+    private String telegramChatId;
+    private Boolean emailNotificationEnabled;
+    private Boolean telegramNotificationEnabled;
+    private String timezone;
+    private LocalDateTime createdAt;
 }
