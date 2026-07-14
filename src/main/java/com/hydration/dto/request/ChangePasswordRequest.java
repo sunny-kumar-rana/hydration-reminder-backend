32b475dcdbd7e2c14f9bb85916dd5715ab3b9bba
Sunny <sunnysinghhrana@gmail.com>
2026-07-14 22:28:50 +0530

    -add ChangePasswordRequest

---
+++ src/main/java/com/hydration/dto/request/ChangePasswordRequest.java
@@ -0,0 +1,23 @@
+package com.hydration.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ChangePasswordRequest {
+
+    @NotBlank
+    @Size(min = 5)
+    private String oldPassword;
+
+    @NotBlank
+    @Size(min = 5)
+    private String newPassword;
+}
