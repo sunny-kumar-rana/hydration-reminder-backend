10fcb53371a25cfe0e1b5ce57cc16533b004a673
Sunny <sunnysinghhrana@gmail.com>
2026-07-14 22:28:40 +0530

    -add UpdateProfileRequest

---
+++ src/main/java/com/hydration/dto/request/UpdateProfileRequest.java
@@ -0,0 +1,25 @@
+package com.hydration.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UpdateProfileRequest {
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotNull
+    private Integer dailyGoal;
+
+    @NotBlank
+    private String timezone;
+}
