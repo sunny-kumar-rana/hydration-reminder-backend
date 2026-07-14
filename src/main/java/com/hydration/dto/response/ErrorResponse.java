45d49a0f33536d6c1fee46b5747bbea99cb008f9
Sunny <sunnysinghhrana@gmail.com>
2026-07-14 22:27:49 +0530

    -update structure

--- src/main/java/com/hydration/dto/response/ErrorResponse.java
+++ src/main/java/com/hydration/dto/response/ErrorResponse.java
@@ -1,5 +1,6 @@
-package com.hydration.dto;
+package com.hydration.dto.response;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -7,6 +8,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
 public class ErrorResponse {
     private String message;
     private int status;
