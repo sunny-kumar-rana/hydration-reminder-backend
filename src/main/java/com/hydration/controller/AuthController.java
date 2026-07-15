175646370c92864707189866262ea8a12bbcd7c5
Sunny <sunnysinghhrana@gmail.com>
2026-07-15 23:20:41 +0530

    -add register to AuthController also rename LoginController.java to AuthController.java

--- src/main/java/com/hydration/controller/AuthController.java
+++ src/main/java/com/hydration/controller/AuthController.java
@@ -1,19 +1,23 @@
 package com.hydration.controller;
 
 import com.hydration.dto.request.LoginRequest;
+import com.hydration.dto.request.RegisterRequest;
 import com.hydration.dto.response.LoginResponse;
-import com.hydration.service.UserService;
+import com.hydration.dto.response.RegisterResponse;
+import com.hydration.service.interfaces.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-public class LoginController {
+@RequestMapping("/api/auth")
+public class AuthController {
     private final UserService userService;
 
     @PostMapping("/login")
@@ -25,5 +29,10 @@ public class LoginController {
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(response);
+    }
+
+    @PostMapping("/register")
+    public RegisterResponse registerUser(@RequestBody @Valid RegisterRequest request){
+        return userService.register(request);
     }
 }
