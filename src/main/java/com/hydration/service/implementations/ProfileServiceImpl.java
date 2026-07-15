3f95782fd25a4feb15eac7bf69ea5429d8b6ff24
Sunny <sunnysinghhrana@gmail.com>
2026-07-15 23:21:36 +0530

    -create ProfileServiceImpl for ProfileService

---
+++ src/main/java/com/hydration/service/implementations/ProfileServiceImpl.java
@@ -0,0 +1,23 @@
+package com.hydration.service.implementations;
+
+import com.hydration.dto.request.ChangePasswordRequest;
+import com.hydration.dto.request.UpdateProfileRequest;
+import com.hydration.dto.response.ProfileResponse;
+import com.hydration.service.interfaces.ProfileService;
+
+public class ProfileServiceImpl implements ProfileService {
+    @Override
+    public ProfileResponse getProfile() {
+        return null;
+    }
+
+    @Override
+    public ProfileResponse updateProfile(UpdateProfileRequest request) {
+        return null;
+    }
+
+    @Override
+    public void changePassword(ChangePasswordRequest request) {
+
+    }
+}
