3f56adab468d5c6f44db99e1d16b4c85e754d807
Sunny <sunnysinghhrana@gmail.com>
2026-07-15 23:19:55 +0530

    -remove register from UserController

--- src/main/java/com/hydration/controller/UserController.java
+++ src/main/java/com/hydration/controller/UserController.java
@@ -1,26 +1,15 @@
 package com.hydration.controller;
 
-import com.hydration.dto.request.RegisterRequest;
-import com.hydration.dto.response.RegisterResponse;
-import com.hydration.service.UserService;
-import jakarta.validation.Valid;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import com.hydration.service.interfaces.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/auth")
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
 public class UserController {
 
     private final UserService userService;
 
-    public UserController(UserService userService){
-        this.userService = userService;
-    }
-
-    @PostMapping("/register")
-    public RegisterResponse registerUser(@RequestBody @Valid RegisterRequest request){
-        return userService.register(request);
-    }
 }
