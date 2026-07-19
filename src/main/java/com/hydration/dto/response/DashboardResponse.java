63b704234056d6141fc19336c798e034df222fac
Sunny <sunnysinghhrana@gmail.com>
2026-07-19 23:40:38 +0530

    -add DashboardResponse

---
+++ src/main/java/com/hydration/dto/response/DashboardResponse.java
@@ -0,0 +1,27 @@
+package com.hydration.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class DashboardResponse {
+
+    private Integer dailyGoal;
+
+    private Integer todayConsumed;
+
+    private Integer remaining;
+
+    private Double progressPercentage;
+
+    private Integer todayEntries;
+
+    private Integer currentStreak;
+
+    private Integer longestStreak;
+}
