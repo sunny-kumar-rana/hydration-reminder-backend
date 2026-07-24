765a305aebfd8438b936f3c4eb127df4bd7cf342
Sunny <sunnysinghhrana@gmail.com>
2026-07-24 23:15:10 +0530

    -add HydrationReminderScheduler

---
+++ src/main/java/com/hydration/scheduler/HydrationReminderScheduler.java
@@ -0,0 +1,20 @@
+package com.hydration.scheduler;
+
+import com.hydration.service.interfaces.ReminderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class HydrationReminderScheduler {
+
+    private final ReminderService reminderService;
+
+    @Scheduled(cron = "0 */1 * * * *")
+    public void scheduleHydrationReminders() {
+
+        reminderService.sendHydrationReminders();
+
+    }
+}
