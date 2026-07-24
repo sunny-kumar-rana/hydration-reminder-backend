96af47c3e67f231887fd001202e79bba5975afa2
Sunny <sunnysinghhrana@gmail.com>
2026-07-24 23:11:25 +0530

    -add EmailServiceImpl

---
+++ src/main/java/com/hydration/service/implementations/EmailServiceImpl.java
@@ -0,0 +1,56 @@
+package com.hydration.service.implementations;
+
+import com.hydration.service.interfaces.EmailService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class EmailServiceImpl implements EmailService {
+
+    private final JavaMailSender mailSender;
+
+    @Value("${spring.mail.username}")
+    private String from;
+
+    @Override
+    public void sendHydrationReminder(String to, String username) {
+
+        SimpleMailMessage message = new SimpleMailMessage();
+
+        message.setFrom(from);
+        message.setTo(to);
+        message.setSubject("💧 Hydration Reminder");
+
+        message.setText(
+                "Hello " + username + ",\n\n" +
+                        "This is a reminder to drink some water.\n\n" +
+                        "Stay hydrated and have a great day!\n\n" +
+                        "- Hydration Reminder"
+        );
+
+        mailSender.send(message);
+    }
+
+    @Override
+    public void sendGoalAchievedEmail(String to, String username) {
+
+        SimpleMailMessage message = new SimpleMailMessage();
+
+        message.setFrom(from);
+        message.setTo(to);
+        message.setSubject("🎉 Daily Goal Achieved");
+
+        message.setText(
+                "Congratulations " + username + "!\n\n" +
+                        "You have successfully achieved your daily hydration goal today.\n\n" +
+                        "Keep up the great work!\n\n" +
+                        "- Hydration Reminder"
+        );
+
+        mailSender.send(message);
+    }
+}
