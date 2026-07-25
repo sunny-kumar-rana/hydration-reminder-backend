53df52f0265b8c338b6e68a64508f17c00870fd0
Sunny <sunnysinghhrana@gmail.com>
2026-07-25 22:15:51 +0530

    -refactor HydrationReminderApplication

--- src/main/java/com/hydration/HydrationReminderApplication.java
+++ src/main/java/com/hydration/HydrationReminderApplication.java
@@ -2,8 +2,10 @@ package com.hydration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class HydrationReminderApplication {
 
     public static void main(String[] args) {
