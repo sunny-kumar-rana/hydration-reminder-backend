2be6856150ff9eaf52e04084e97f27ac06052757
Sunny <sunnysinghhrana@gmail.com>
2026-08-06 23:08:57 +0530

    -cleanup

--- src/main/java/com/hydration/config/SecurityConfig.java
+++ src/main/java/com/hydration/config/SecurityConfig.java
@@ -48,11 +48,10 @@ public class SecurityConfig {
 
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
-                                "/api/auth/**",
-                                "/api/test/**",
-                                "/swagger-ui/**",
-                                "/swagger-ui.html",
-                                "/v3/api-docs/**"
+                                "/api/auth/**"
+//                                ,"/swagger-ui/**",
+//                                "/swagger-ui.html",
+//                                "/v3/api-docs/**"
                         ).permitAll()
                         .anyRequest().authenticated())
 
